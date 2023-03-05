7a4275442a7b01c6035e03fe6a5a989346fe2fc1
Muhammad Osama <m.osama169@gmail.com>
2023-03-05 12:05:11 +0300

    style: add styling to input and cards

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import image1 from '../assets/space-water.png'
-import image2 from '../assets/fantacy-city.png'
-import image3 from '../assets/night-wild.png'
+import image2 from '../assets/night-wild.png'
+import image3 from '../assets/girl-riding-cat.png'
+import image4 from '../assets/mexican-face-girl.jfif'
+import image5 from '../assets/fantacy-city.png'
+
 
 export const Gallery = () => {
   return (
-    <div className="grid md:grid-cols-2 p-10 justify-center gap-5">
-        <div className="grid grid-col-[10px_10px]">
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
-            {/* <img className="w-[50%]" src={image2}/>
-            <img className="w-[50%]" src={image3}/> */}
+    <div className="flex flex-row gap-5 justify-center">
+
+      <div className="w-[600px] flex">
+        <img className="rounded-xl" src={image1}/>
+      </div>
+
+      <div className="w-[400px] flex">
+
+        <div className="flex flex-col w-[600px]">
+
+          <div className="flex flex-row gap-5 justify-between mb-5">
+            <img className="w-[200px] h-[200px] rounded-xl" src={image4}/>
+            <img className="w-[180px] h-[200px] rounded-xl" src={image3}/>
+          </div>
+          
+          <img className="rounded-xl" src={image2}/>
+
         </div>
-        <div>
-            <img src={image1}/>
-        </div>
+      </div>
 
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -34,13 +34,23 @@ const Search = ({setListImages,setIsLoading}) => {
   }
 
   return (
-    <form className="relative my-8 max-w-md" onSubmit={handleSubmit}>
-        <input value={InputValue} onChange={handleInputChange} className="indent-2 py-4 pr-[4.5rem] rounded-sm bg-gray-50 w-full md:max-w-md focus:ring-primary outline-none focus:ring-1" type="text" placeholder="Unleash the power of AI to create" />
-        <button type="submit" className="absolute bg-red-600 bottom-2.5 right-2.5 bg-primary text-white px-2 py-2 rounded-sm text-sm">
-            Generate
-        </button>
-        
+    <div className="flex flex-col justify-center mt-10">
+    <h1 className="font-opensans text-[35px] text-white text-center">Give It A <span className="font-opensans text-[35px] text-[#19F9FF]">Go</span></h1>
+      <form className=" my-8 max-full" onSubmit={handleSubmit}>
+        <div className="flex flex-row justify-center ">
+          <input value={InputValue}
+          onChange={handleInputChange}
+          type="text" 
+          placeholder="Two Monkeys sitting in first class on a plane, wearing sunglasses and sipping on cocktails. 3D Realistic" 
+          className="flex flex-col rounded-l-lg indent-2 py-4 rounded-sm placeholder-gray-500 md:w-[800px] px-3 pl-8 outline-none focus:shadow-outline " />
+
+          <button type="submit" className=" bg-[#19F9FF] bottom-2.5 right-2.5 text-black px-2 py-2 rounded-r-lg text-md">
+              Generate
+          </button>
+      </div>
     </form>
+    </div>
+
   )
 }
 

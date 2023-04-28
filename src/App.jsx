8fb5bdafbe948239366ed9f7db7752c580281381
SilverousMuhammad169 <silverousspy169@gmail.com>
2023-04-28 17:37:47 +0300

    feat: add authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { SignInPage } from './components/SignInPage';
-import { useDispatch } from 'react-redux';
-import { loginSuccess } from '../store/UserSlice';
-import { useEffect } from 'react';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const getUser = () => {
-      fetch('http://localhost:5000/auth/login/success', {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Credentials': true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error('authentication has been failed!');
-        })
-        .then((resObject) => {
-          dispatch(loginSuccess(resObject.user));
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
-
   return (
     <Router>
       <Routes>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+dotenv.config();
+
+export default {
   plugins: [react()],
-});
+  define: {
+    'process.env': process.env,
+  },
+};
